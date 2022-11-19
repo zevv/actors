256430365a439ef4f47f57c151487223224336d5
Ico Doornekamp <ico@zevv.nl>
2022-11-19 20:07:48 +0100

    findActor

--- actor.nim
+++ actor.nim
@@ -11,10 +11,14 @@ type
   Message* {.acyclic.} = ref object of RootObj
     src*: string
 
-  Actor* = ref object
+  ActorObject* = object
     name: string
     thread: Thread[Actor]
     mailbox: Channel[Message]
+
+  Actor* = ref ActorObject
+
+  ActorPtr* = ptr ActorObject
 
   ActorPool = object
     lock: Lock
@@ -33,25 +37,30 @@ proc spawn*(name: string, fn: ActorProc) =
   withLock pool.lock:
     pool.actors[name] = a
 
+  
+# Look up the actor in the global `actors` table. `actors` is owned by the main
+# thread so we need a gcsafe cast and a raw pointer to be able to be able to
+# use it and make sure the calling thread does not touch the RC.
+
+proc findActor(name: string): ActorPtr =
+  {.cast(gcsafe).}:
+    var dst {.cursor.}: Actor
+    withLock pool.lock:
+      if name in pool.actors:
+        result = pool.actors[name][].unsafeAddr
+
 
 proc send*(a: Actor, name: string, m: sink Message) =
   # Set the source name of the message
   m.src = a.name
   # It is only safe to send the message if ours is the only reference to it
   verifyIsolated(m)
-  # Look up the actor in the global `actors` table. `actors` is owned by the
-  # main thread so we need a gcsafe cast and cursor to be able to be able to
-  # use it and make sure the calling thread does not touch the RC.
-  {.cast(gcsafe).}:
-    var dst {.cursor.}: Actor
-    withLock pool.lock:
-      if name in pool.actors:
-        dst = pool.actors[name]
-    if dst != nil:
-      dst.mailbox.send(m)
-      # Tell nim that the object moved away and it should not touch the RC
-      # after this anymore
-      wasmoved(m)
+  var dst = findActor(name)
+  if dst != nil:
+    dst.mailbox.send(m)
+    # Tell nim that the object moved away and it should not touch the RC
+    # after this anymore
+    wasmoved(m)
 
 
 proc recv*(a: Actor): Message =
