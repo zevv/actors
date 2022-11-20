46423e9826a26fdb5fe2d6e160c8c50671f5e06d
Smooth Operator <github@andy.disruptek.com>
2022-11-19 21:01:32 -0500

    tweaks

--- actors.nim
+++ actors.nim
@@ -1,8 +1,8 @@
 
-import tables
-import std/locks
-import isisolated
+import std/tables
+import std/rlocks
 
+import isisolated
 
 type
 
@@ -11,56 +11,60 @@ type
   Message* {.acyclic.} = ref object of RootObj
     src*: string
 
-  ActorObject* = object
+  Actor* = ref object
     name: string
     thread: Thread[Actor]
     mailbox: Channel[Message]
 
-  Actor* = ref ActorObject
-
-  ActorPtr* = ptr ActorObject
-
   ActorPool = object
-    lock: Lock
+    lock: RLock
     actors: Table[string, Actor]
 
-  ActorProc = proc(a: Actor) {.thread,nimcall.}
+  ActorProc = proc(a: Actor) {.thread, nimcall.}
 
 
 var pool: ActorPool
+initRLock pool.lock
 
 
 proc spawn*(name: string, fn: ActorProc) =
   let a = Actor(name: name)
   a.mailbox.open()
   createThread(a.thread, fn, a)
-  withLock pool.lock:
+  withRLock pool.lock:
     pool.actors[name] = a
 
-  
+
 # Look up the actor in the global `actors` table. `actors` is owned by the main
 # thread so we need a gcsafe cast and a raw pointer to be able to be able to
 # use it and make sure the calling thread does not touch the RC.
 
-proc findActor(name: string): ActorPtr =
+proc findActor(name: string): Actor =
   {.cast(gcsafe).}:
-    var dst {.cursor.}: Actor
-    withLock pool.lock:
+    withRLock pool.lock:
       if name in pool.actors:
-        result = pool.actors[name][].unsafeAddr
+        result = pool.actors[name]
 
+template ifItIsActorNamed(name: string; body: untyped): untyped =
+  {.cast(gcsafe).}:
+    withRLock pool.lock:
+      var it {.inject.} = findActor(name)
+      if not it.isNil:
+        body
 
 proc send*(a: Actor, name: string, m: sink Message) =
+  # Tell nim that the object moved away and it should not touch the RC
+  # after this procedure
+  defer:
+    wasmoved(m)
   # Set the source name of the message
   m.src = a.name
   # It is only safe to send the message if ours is the only reference to it
   verifyIsolated(m)
-  var dst = findActor(name)
-  if dst != nil:
-    dst.mailbox.send(m)
-    # Tell nim that the object moved away and it should not touch the RC
-    # after this anymore
-    wasmoved(m)
+  ifItIsActorNamed name:
+    it.mailbox.send(m)
+    return
+  raise ValueError.newException name & " is dead"
 
 
 proc recv*(a: Actor): Message =
