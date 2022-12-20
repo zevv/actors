908046d78e301be5e727a55d157f987fabcb87c2
Ico Doornekamp <ico@zevv.nl>
2022-12-20 19:37:44 +0100

    Smaller

--- tests/valgrind.nim
+++ tests/valgrind.nim
@@ -2,7 +2,5 @@
 {.emit: "#include <valgrind/valgrind.h>".}
 
 proc running_on_valgrind*(): bool =
-  {.emit: """
-    result = RUNNING_ON_VALGRIND;
-  """.}
+  {.emit: "result = RUNNING_ON_VALGRIND;".}
 
