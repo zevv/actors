367e9ca891e47d793f8dd0ee5b3e554ec3b44a03
Ico Doornekamp <ico@zevv.nl>
2023-02-11 15:59:54 +0100

    tsigsegv is the smallest reproducable crash

--- tests/tsigsegv.nim
+++ tests/tsigsegv.nim
@@ -7,12 +7,15 @@ type
   Ping = ref object of Message
 
 proc alice() {.actor.} =
-  alice.send(Ping())
   var req = recv()
+
+proc bob(alice: Actor) {.actor.} =
+  alice.send(Ping())
 
 proc main() =
   let pool = newPool(4)
   let a = pool.hatch alice()
+  let b = pool.hatch bob(a)
   pool.join()
 
 main()
