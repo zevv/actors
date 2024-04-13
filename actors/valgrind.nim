c2fb46afa268141f6fa9adbd4c3de262fe6fb6c5
zerbina <100542850+zerbina@users.noreply.github.com>
2024-04-13 17:23:15 +0000

    add the `usesValgrind` conditional symbol
    Valgrind-specific code is only included in the build if it's present.
    
    `usesValgrind` is defined for the files in the test director.

--- actors/valgrind.nim
+++ actors/valgrind.nim
@@ -1,20 +1,29 @@
 
+const
+  usesValgrind {.booldefine.} = false
+
 {.emit:"#include <valgrind/helgrind.h>".}
 
 template valgrind_annotate_happens_before*(x) =
   block:
     let y {.exportc,inject.} = x
-    {.emit:"ANNOTATE_HAPPENS_BEFORE(y);".}
+    when usesValgrind:
+      {.emit:"ANNOTATE_HAPPENS_BEFORE(y);".}
 
 template valgrind_annotate_happens_after*(x) =
   block:
     let y {.exportc,inject.} = x
-    {.emit:"ANNOTATE_HAPPENS_AFTER(y);".}
+    when usesValgrind:
+      {.emit:"ANNOTATE_HAPPENS_AFTER(y);".}
 
 template valgrind_annotate_happens_before_forget_all*(x) =
   block:
     let y {.exportc,inject.} = x
-    {.emit:"ANNOTATE_HAPPENS_BEFORE_FORGET_ALL(y);".}
+    when usesValgrind:
+      {.emit:"ANNOTATE_HAPPENS_BEFORE_FORGET_ALL(y);".}
 
 proc running_on_valgrind*(): bool =
-  {.emit: "result = RUNNING_ON_VALGRIND;".}
+  when usesValgrind:
+    {.emit: "result = RUNNING_ON_VALGRIND;".}
+  else:
+    false
